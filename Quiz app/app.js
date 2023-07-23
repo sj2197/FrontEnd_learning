9057455392adafeddcdbb4bd148957c40fd7172f
sj2197 <jannatsifate@gmail.com>
2023-07-23 20:19:09 +0900

    Initial commit Add project Quiz app

---
+++ Quiz app/app.js
@@ -0,0 +1,38 @@
+const question = [
+    {
+        question: "Which is the largest animal in the world?",
+        answer:[
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answer:[
+            {text: "Vietnam", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Sri lanka", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answer:[
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answer:[
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    },
+];
